43696141b8892423cee30008884c68b8f4a8e7a6
sikandarmushtaq <raisikandar502@gmail.com>
2026-08-31 07:42:30 +0500

    Update portfolio image

--- my-app/src/data/TeamData.js
+++ my-app/src/data/TeamData.js
@@ -1,5 +1,7 @@
+import sikandar from "../Team Images/sikandar.PNG"
+
 export const teamMembers = [
-  { name: 'Sikandar Mushtaq', role: 'CEO / Lead Full-Stack Engineer', img: '' },
+  { name: 'Sikandar Mushtaq', role: 'Cerified Full-Stack Engineer', img: {sikandar} },
   { name: 'Ali Raza',         role: 'Creative Developer',             img: '' },
   { name: 'Ayesha Khan',      role: 'UI/UX Designer',                 img: '' },
   { name: 'Bilal Ahmed',      role: '3D Artist',                      img: '' },
