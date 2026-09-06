a0f9a8326e2a4d42651f425a4319136204f257d8
sikandarmushtaq <raisikandar502@gmail.com>
2026-09-06 22:28:18 +0500

    added new team pic

--- my-app/src/data/TeamData.js
+++ my-app/src/data/TeamData.js
@@ -1,8 +1,9 @@
 import sikandar from "../Team Images/sikandar.PNG"
+import zaman from "../Team Images/zaman.png"
 
 export const teamMembers = [
   { name: 'Sikandar Mushtaq', role: 'Certified Full-Stack Engineer/3D Web Developer', img: sikandar },
-  { name: 'Zaman Mushtaq',         role: 'Full-Stack MERN/App Developer',             img: '' },
+  { name: 'Zaman Mushtaq',         role: 'Full-Stack MERN/App Developer',             img: zaman },
   { name: 'Muhammad Abdullah ',      role: 'AI Engineer',                 img: '' },
   { name: 'Bilal Ahmed',      role: 'UI/UX Designer',                      img: '' },
   { name: 'Usman Tariq',      role: 'AI Engineer',                    img: '' },  
